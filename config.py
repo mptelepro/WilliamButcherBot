ff77eaf910b4f2475367d644996edec66b7d99c1
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-20 22:30:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-bot_token = "1297166370:AAF3oSPlzBIzzv-s3oCbKQ7QDjKw0XzXW5M"
+bot_token = "1778855883:AAFLLAD4JiknxzGN3TGn1tVsZXSFgnRSFWI"
 
-api_id = 1749924
+api_id = 3763044
 
-api_hash = "87e4a056bb574f7939ac283b9a9187ab"
+api_hash = "b13e001179477b80af61ae0d7e0c1064"
 
-OWNER_ID = 984441749
+OWNER_ID = 1467872591
 
-sudo_users_id = [984441749, 984441749, 984441749]
+sudo_users_id = [1467872591, 984441749, 984441749]
 
-log_group_id = -1001367221700
+log_group_id = --1001172200823
 
 # https://cryptography.io/en/latest/fernet.html
 fernet_encryption_key = "iKMq0WZMnJKjMQxZWKtv-cplMuF_LoyshXj0XbTGGWM="
